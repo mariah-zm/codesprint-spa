bd4818713d294c643319ff9672355095472f30a3
mariahz <mariahz@ccbill.com>
2022-07-15 14:58:03 +0200

    Adds explore quizzes page

---
+++ src/app/core/models/quiz-response.ts
@@ -0,0 +1,13 @@
+export class QuizResponse {
+    id!: number;
+    name!: string;
+    description!: string;
+    amountQuestions!: number;
+
+    constructor(id: number, name: string, desc: string, numQues: number) {
+        this.id = id;
+        this.name = name;
+        this.description = desc;
+        this.amountQuestions = numQues;
+    }
+}
--- src/app/modules/explore/explore.component.ts
+++ src/app/modules/explore/explore.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { QuizResponse } from 'src/app/core/models/quiz-response';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html'
 })
 export class ExploreComponent implements OnInit {
+  quizzes: QuizResponse[] = [];
 
-  constructor() { }
+  constructor() {
+    // To replace this by retrieving quizzes from BE
+
+    const quizResponse = new QuizResponse(
+      1,
+      "Quiz Title",
+      "This is an example of a quiz title in which the teacher can explain what the quiz is about.",
+      10
+    );
+
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+    this.quizzes.push(quizResponse);
+  }
 
   ngOnInit(): void {
   }
