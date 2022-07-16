a7f8fefef647c180b7eb7db49097b430ce2c32f7
mariahz <mariahz@ccbill.com>
2022-07-16 07:46:49 +0200

    Modifies login page

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -10,43 +10,21 @@ import { AuthenticationService } from 'src/app/core/services/authentication.serv
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;    // ! means will be declared later on during execution
-  loginErrors: string[] = [];
-  loginFormSubmitted: boolean = false;
   isLoading: boolean = false;
   isAlertClosed: boolean = false;
   isRedirected!: boolean;
 
-  get f() { return this.loginForm.controls; }
-
   constructor(
-    private formBuilder: FormBuilder,
     private authService: AuthenticationService,
-    private router: Router) {
-      if (this.router.getCurrentNavigation()!.extras.state == undefined)
-        this.isRedirected = false;
-      else 
+    private router: Router
+  ) {
+    if (this.router.getCurrentNavigation()!.extras.state == undefined)
+      this.isRedirected = false;
+    else
       this.isRedirected = this.router.getCurrentNavigation()!.extras.state!['isRedirected'];
-    }
-
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    })
   }
 
-  onSubmit(): void {
-    this.loginErrors = [];
-    this.loginFormSubmitted = true;
-
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.isLoading = true;
-
-    this.isLoading = false;
+  ngOnInit(): void {
   }
 
   onFacebookLogin(): void {
